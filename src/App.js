f21a1642928cfc03031b03558fa9c47318eaa9d4
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-30 16:34:12 +0530

    Completed the given task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-                <Routes>
+  const [data, setData] = useState([]);
 
-				    <Route path='/login' element={<Login />}></Route>
-				    <Route path='/register' element={<Register />}></Route>
-			
-			
-                </Routes>
-			</BrowserRouter>
-		</div>
-	)
+  // const debouncing = (cb, delay) => {
+  // 	let timer;
+  // 	return (...args) => {
+  // 		console.log('herer')
+  // 		if (timer) {
+  // 			clearTimeout(timer);
+  // 		}
+  // 		timer = setTimeout(() => {
+  // 			cb(...args);
+  // 		}, delay);
+  // 	}
+  // };
+
+  const getData = async (search = null) => {
+    try {
+      let res = {};
+      if (search) {
+        res = await axios(
+          `https://jsonplaceholder.typicode.com/todos?q=${search}`
+        );
+      } else {
+        res = await axios("https://jsonplaceholder.typicode.com/todos");
+      }
+      console.log(res.data);
+      setData(res.data);
+    } catch (error) {
+      console.log("Error:-" + error);
+    }
+  };
+
+  let timer;
+  const handleSearch = (value) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        getData(value);
+      }, 2000);
 }
 
-export default App
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <input type="text" className="inputBar" onChange={e=>handleSearch(e.target.value)} />
+      {data.map((todo) => (
+        <div key={todo.id}>{todo.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
